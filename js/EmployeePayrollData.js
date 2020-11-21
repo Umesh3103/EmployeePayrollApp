29f716203fc9284ac02458e3ab9d68f37e06e917
DESKTOP-1260D35\Hp <deoraumesh32@gmail.com>
2020-11-21 16:01:21 +0530

    [Umesh]Added: added more attributes according to the form

---
+++ js/EmployeePayrollData.js
@@ -0,0 +1,55 @@
+class EmployeePayrollData {
+
+    get name() { return this._name; }
+    set name(name) { 
+        let nameRegex = RegExp('^[A-Z]{1}[a-zA-Z\\s]{2,}$');
+        if(nameRegex.test(name))
+            this._name = name;
+        else throw "Name is incorrect";    
+         
+    }
+
+    get profilePic() { return this._profilePic;}
+    set profilePic(profilePic) {
+        this._profilePic=profilePic;
+    }
+
+    get gender() { return this._gender;}
+    set gender(gender) {
+        let genderRegex = RegExp('^[M F]$');
+        if(genderRegex.test(gender))
+            this._gender=gender;
+        else throw "Gender is incorrect";    
+
+    }
+
+    get department() {return this._department;}
+    set department(department) {
+        this._department=department;
+    }
+
+    get salary() { return this._salary;}
+    set salary(salary) {
+        let salaryRegex = RegExp('^[1-9]+[0-9]*$');
+        if(salaryRegex.test(salary))
+            this._salary = salary;
+        else throw "salary is incorrect";
+    }
+
+    get note() { return this._note;}
+    set note(note) {
+        this._note=note;
+    }
+    get startDate() { return this._startDate;}
+    set startDate(startDate) {
+        this._startDate=startDate;
+    }
+    
+    toString() {
+        const options = { year: "numeric", month: "long", day: "numeric"};
+        const empDate = this.startDate === undefined ? undefined :
+                        this.startDate.toLocaleDateString("en-US",options);
+        return "name="+this.name+", gender="+this.gender+", profilePic="+this.profilePic+", department="+this.department+", salary="+this.salary+", startDate="+empDate
+        +", note="+this.note;
+    }
+}
--- js/payroll_script.js
+++ js/payroll_script.js
@@ -5,4 +5,40 @@
     salary.addEventListener('input', function() {
         output.textContent = salary.value;
     });
+
+    const day = document.querySelector('#day');
+    const month = document.querySelector('#month');
+    const year = document.querySelector('#year');
+    const dateError = document.querySelector('.date-error');
+    let dateArr= [day,month,year];
+    dateArr.forEach(data=> data.addEventListener('input',function() {
+        if(month.value == 1){
+            if(isYearLeap(year.value)){
+                if(day.value>29)
+                    dateError.textContent = "Date is Invalid";
+                else dateError.textContent="";    
+            }
+            else{
+                if(day.value>28)
+                    dateError.textContent = "Date is Invalid";
+                else dateError.textContent="";    
+            }
+        }
+        if(month.value == 3 || month.value == 5 || month.value == 8 || month.value ==10)
+            if(day.value>30){
+                dateError.textContent = "Date is Invalid";
+            } else dateError.textContent="";
+    }));
+
+    const isYearLeap = (year) => {
+        if(year%4==0){
+            if(year%100==0){
+                if(year%400==0){
+                    return true;
+                }
+            }
+            else return true;
+        }
+        return false;
+    }
 }
